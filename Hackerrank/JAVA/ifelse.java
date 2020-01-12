79588f460feebe6b37c4958fa5eaeea09fa8eda9
VivekAlhat <vivekalhat14@outlook.com>
2020-01-12 23:19:16 +0530

    Added gitignore

--- Hackerrank/JAVA/ifelse.java
+++ Hackerrank/JAVA/ifelse.java
@@ -5,3 +5,22 @@
 	If n is even and in the inclusive range of  to , print Weird
 	If n is even and greater than , print Not Weird
 */
+
+import java.util.*;
+
+class ifelse{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		if(n%2!=0){
+			System.out.println("Weird");
+		}else if(n%2==0 && n>=2&&n<=5){
+			System.out.println("Not Weird");
+		}else if(n%2==0 && n>=6&&n<=20){
+			System.out.println("Weird");
+		}else if(n%2==0 && n>20){
+			System.out.println("Not Weird");
+		}
+	}
+}
