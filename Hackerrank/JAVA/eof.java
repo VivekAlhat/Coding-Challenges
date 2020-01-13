8d202a653de29767bdcd252f85a97cc351781ea5
VivekAlhat <vivekalhat14@outlook.com>
2020-01-13 14:28:50 +0530

    Added eof challenge

---
+++ Hackerrank/JAVA/eof.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class eof{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int i=1;
+		while(sc.hasNext()){
+			String s = sc.nextLine();
+			System.out.println(i+" "+s);
+			i += 1;
+		}
+	}
+}
