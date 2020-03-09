7b610d504a7bf6fdb79b89f21b4ec94c0c5ea2fc
VivekAlhat <vivekalhat14@outlook.com>
2020-03-09 23:40:40 +0530

    Update Data

--- Codechef/engxor.cpp
+++ Codechef/engxor.cpp
@@ -1,37 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-
-int main(int argc, const char** argv){
-	ll even=0, odd=0;
-	ll t, n, q, el;
+int main(){
+	ll t;
 	scanf("%lld", &t);
 	while(t-->0){
-		int e,o;
+		ll n, q;
+		ll cnt = 0;
 		scanf("%lld %lld", &n, &q);
-		ll data[n];
-		for(ll i=0;i<n;++i){
-			scanf("%lld", &data[i]);
-			int count = __builtin_popcount(data[i]);
-			if(count%2==0){
-				even += 1;
-			}else{
-				odd += 1;
-			}
+		ll vec[n];
+		for(int i=0;i<n;++i){
+			scanf("%lld", &vec[i]);
+			if(__builtin_popcount(vec[i])%2==0)
+				cnt++;
 		}
-		
-		for(ll i=0;i<q;++i){
+		while(q-->0){
+			ll el;
 			scanf("%lld", &el);
-			int count = __builtin_popcount(el);
-			if(count%2==0){
-				e = even;
-				o = odd;
+			ll elCnt = __builtin_popcount(el);
+			if(elCnt%2==0){
+				cout<<cnt<<" "<<n-cnt<<endl;
 			}else{
-				o = even;
-				e = odd;
+				cout<<n-cnt<<" "<<cnt<<endl;
 			}
-			cout<<e<<" "<<o<<endl;
 		}
 	}
 	return 0;
 }
+
