082d1d058734474700d2cb2bde18a6231863734a
VivekAlhat <vivekalhat14@outlook.com>
2020-03-29 03:19:52 +0530

    Added 1030A Solution

---
+++ Codeforces/1030A.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, flag = 0;
+    cin >> n;
+    int arr[n];
+    for (size_t i = 0; i < n; i++)
+    {
+        /* code */
+        cin >> arr[i];
+    }
+
+    for (size_t i = 0; i < n; i++)
+    {
+        /* code */
+        if (arr[i] == 1)
+        {
+            /* code */
+            flag = 1;
+            break;
+        }
+        else
+        {
+            /* code */
+            flag = 0;
+        }
+    }
+
+    cout << (flag == 1 ? "HARD" : "EASY") << endl;
+    return 0;
+}
