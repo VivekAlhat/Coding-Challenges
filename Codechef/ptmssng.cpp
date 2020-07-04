2c5014a894a7de086e9127dfff29c1ae8ea5b171
VivekAlhat <vivekalhat14@outlook.com>
2020-07-05 00:38:43 +0530

    PTMMSNG Solution

---
+++ Codechef/ptmssng.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        ll n, a, b;
+        vector<ll> pointX;
+        vector<ll> pointY;
+        cin >> n;
+        n = (4 * n) - 1;
+        for (int i = 0; i < n; ++i)
+        {
+            cin >> a >> b;
+            pointX.push_back(a);
+            pointY.push_back(b);
+        }
+        ll x = pointX[0];
+        ll y = pointY[0];
+        for (int i = 1, j = 1; i < pointX.size() && j < pointY.size(); ++i, ++j)
+        {
+            x = x ^ pointX.at(i);
+            y = y ^ pointY.at(j);
+        }
+        cout << x << " " << y << endl;
+    }
+    return 0;
+}
