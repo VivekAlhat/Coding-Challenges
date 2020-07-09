3bb43c4de3479d5822aad178afff1e59d6082a72
VivekAlhat <vivekalhat14@outlook.com>
2020-07-09 16:03:39 +0530

    Add them twice solution

---
+++ Codeforces/addthemtwice.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    double a, b, c;
+    cin >> a >> b >> c;
+    cout << fixed << setprecision(6) << a * 2 + b * 2 + c * 2 << endl;
+    return 0;
+}
